5e4c2686fac6f97aafbfabc03a6eb93ba01ec63e
Kyle <wongyeowkin@gmail.com>
2023-12-27 04:54:57 -0800

    refactor codes

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -27,52 +27,61 @@
 	};
 	programs = {
 		home-manager.enable = true;
-		bat.enable = true;
-		bat.config.theme = "TwoDark";
-		fzf.enable = true;
-		fzf.enableZshIntegration = true;
-		lsd.enable = true;
-		lsd.enableAliases = true;
+        bat = {
+            enable = true;
+            };
+        fzf = {
+            enable = true;
+            enableZshIntegration = true;
+        };
+        lsd = {
+            enable = true;
+            enableAliases = true;
+        };
 		git = {
 		  enable = true;
 		  userName = "Kyle";
 		  userEmail = "wongyeowkin@gmail.com";
 		};
 		git-credential-oauth.enable = true;
-		zsh.enable = true;
-		zsh.enableCompletion = true;
-		zsh.enableAutosuggestions = true;
-		zsh.syntaxHighlighting.enable = true;
-		zsh.autocd = true;
-		zsh.shellAliases = {
-			nixswitch = "darwin-rebuild switch --flake ~/nix-system-config/.#";
-			nixup = "pushd ~/nix-system-config; nix flake update; nixswitch; popd";
-		};
-        zsh.initExtraFirst = ''
-        if which tmux 2>&1 >/dev/null; then
-            if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
-                # Get the list of tmux sessions
-                sessions=$(tmux list-sessions 2> /dev/null)
-                if [ -n "$sessions" ]; then
-                    # Check if "Hack" session exists in the list
-                    hack_session=$(echo "$sessions" | grep -o 'Hack' | head -n 1)
-                    if [ -n "$hack_session" ]; then
-                        # If "Hack" session exists, attach to it
-                        tmux attach-session -t "Hack"
+        zsh = {
+            enable = true;
+            enableCompletion = true;
+            enableAutosuggestions = true;
+            syntaxHighlighting.enable = true;
+            autocd = true;
+            shellAliases = {
+                nixswitch = "darwin-rebuild switch --flake ~/nix-system-config/.#";
+                nixup = "pushd ~/nix-system-config; nix flake update; nixswitch; popd";
+            };
+            initExtraFirst = ''
+            if which tmux 2>&1 >/dev/null; then
+                if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
+                    # Get the list of tmux sessions
+                    sessions=$(tmux list-sessions 2> /dev/null)
+                    if [ -n "$sessions" ]; then
+                        # Check if "Hack" session exists in the list
+                        hack_session=$(echo "$sessions" | grep -o 'Hack' | head -n 1)
+                        if [ -n "$hack_session" ]; then
+                            # If "Hack" session exists, attach to it
+                            tmux attach-session -t "Hack"
+                        else
+                            # If "Hack" session not found, attach to the first one
+                            first_session=$(echo "$sessions" | awk -F: '{print $1}' | head -n 1)
+                            tmux attach-session -t "$first_session"
+                        fi
                     else
-                        # If "Hack" session not found, attach to the first one
-                        first_session=$(echo "$sessions" | awk -F: '{print $1}' | head -n 1)
-                        tmux attach-session -t "$first_session"
+                        # If no sessions exist, create and attach to "Hack"
+                        tmux new-session -s Hack
                     fi
-                else
-                    # If no sessions exist, create and attach to "Hack"
-                    tmux new-session -s Hack
                 fi
             fi
-        fi
-        '';
-		starship.enable = true;
-		starship.enableZshIntegration = true;
+            '';
+        };
+        starship = {
+            enable = true;
+            enableZshIntegration = true;
+        };
         tmux = {
             enable = true;
             extraConfig = ''
