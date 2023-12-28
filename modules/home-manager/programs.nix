fae94b10722dc31890ec2955271747cb7ededc82
y3owk1n <wongyeowkin@gmail.com>
2023-12-28 21:56:41 +0800

    add more configuration for git

--- modules/home-manager/programs.nix
+++ modules/home-manager/programs.nix
@@ -25,8 +25,29 @@
     };
     git = {
       enable = true;
-      userName = "Kyle";
+      userName = "y3owk1n";
       userEmail = "wongyeowkin@gmail.com";
+      extraConfig =
+        {
+          github.user = "y3owk1n";
+          color.ui = true;
+          pull.rebase = true;
+          merge.conflictstyle = "diff3";
+          init.defaultBranch = "main";
+          http.sslVerify = true;
+          commit.verbose = true;
+          diff.algorithm = "patience";
+          protocol.version = "2";
+          core.commitGraph = true;
+          gc.writeCommitGraph = true;
+          push.autoSetupRemote = true;
+        }
+        // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
+          # these should speed up vim nvim-tree and other things that watch git repos but
+          # only works on mac. see https://github.com/nvim-tree/nvim-tree.lua/wiki/Troubleshooting#git-fsmonitor-daemon
+          core.fsmonitor = true;
+          core.untrackedcache = true;
+        };
     };
     git-credential-oauth.enable = true;
     zsh = {
