ba6213586ed1676c551a048f60cbd233777430b7
y3owk1n <wongyeowkin@gmail.com>
2023-12-29 17:54:34 +0800

    chore: add bat catppuccin theme

--- modules/home-manager/programs.nix
+++ modules/home-manager/programs.nix
@@ -4,13 +4,24 @@
     bat = {
       enable = true;
       config = {
-        theme =
-          "Dracula"; # I like the TwoDark colors better, but want bold/italic in markdown docs
+        theme = "catppuccin";
         #pager = "less -FR";
         pager = "page -WO -q 90000";
         italic-text = "always";
         style =
           "plain"; # no line numbers, git status, etc... more like cat with colors
+      };
+      themes = {
+        catppuccin = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat"; # Bat uses sublime syntax for its themes
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+          file = "Catppuccin-macchiato.tmTheme";
+        };
+
       };
     };
     fzf = {
