6ee611d6308106243946fa9a3b51caf22d0fb4e8
y3owk1n <wongyeowkin@gmail.com>
2023-12-29 16:50:43 +0800

    chore: add fzf catppuccin theme

--- modules/home-manager/programs.nix
+++ modules/home-manager/programs.nix
@@ -17,6 +17,20 @@
       enable = true;
       enableZshIntegration = true;
       tmux.enableShellIntegration = true;
+      colors = {
+        "bg+" = "#363a4f";
+        bg = "#24273a";
+        spinner = "#f4dbd6";
+        hl = "#ed8796";
+        fg = "#cad3f5";
+        header = "#ed8796";
+        info = "#c6a0f6";
+        pointer = "#f4dbd6";
+        marker = "#f4dbd6";
+        "fg+" = "#cad3f5";
+        prompt = "#c6a0f6";
+        "hl+" = "#ed8796";
+      };
       defaultCommand = "fd --type f --hidden --exclude .git";
       fileWidgetCommand =
         "fd --exclude .git --type f"; # for when ctrl-t is pressed
