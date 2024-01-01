3d4ff660bfe70abd8cc3ca76d74a581ba958612f
y3owk1n <wongyeowkin@gmail.com>
2024-01-01 23:08:11 +0800

    chore: remove some git related configs

--- modules/home-manager/programs.nix
+++ modules/home-manager/programs.nix
@@ -58,22 +58,23 @@
       userEmail = "wongyeowkin@gmail.com";
       extraConfig = {
         github.user = "y3owk1n";
-        color.ui = true;
+        # color.ui = true;
         pull.rebase = true;
         merge.conflictstyle = "diff3";
         init.defaultBranch = "main";
         http.sslVerify = true;
-        commit.verbose = true;
-        diff.algorithm = "patience";
+        # commit.verbose = true;
+        # diff.algorithm = "patience";
         protocol.version = "2";
-        core.commitGraph = true;
-        gc.writeCommitGraph = true;
-        push.autoSetupRemote = true;
+        # core.commitGraph = true;
+        # gc.writeCommitGraph = true;
+        # push.autoSetupRemote = true;
       } // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
         # these should speed up vim nvim-tree and other things that watch git repos but
         # only works on mac. see https://github.com/nvim-tree/nvim-tree.lua/wiki/Troubleshooting#git-fsmonitor-daemon
         core.fsmonitor = true;
         core.untrackedcache = true;
+        feature.manyFiles = true;
       };
     };
     git-credential-oauth.enable = true;
