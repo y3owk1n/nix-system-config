d1d9ddb96f0421cf5eb81a3b596044581501572e
y3owk1n <wongyeowkin@gmail.com>
2024-01-04 21:49:01 +0800

    chore: refactor fish functions and add description + autoload

--- modules/home-manager/programs/fish/default.nix
+++ modules/home-manager/programs/fish/default.nix
@@ -259,123 +259,160 @@
         }
       ];
       functions = {
+        __load-em = {
+          description = "Loads Fish shell function descriptions.";
+          body = ''
+            # Load function information so it shows up in auto completion
+            # Original from https://github.com/fish-shell/fish-shell/issues/1915#issuecomment-72315918
+
+            for i in (functions | tr , ' ')
+                functions $i > /dev/null
+            end
+          '';
+        };
         fish_user_key_bindings = ''
           fish_vi_key_bindings
           bind \cg _fzf_dev_folder
           bind -M insert \cg _fzf_dev_folder
         '';
-        mkdirx = ''
-          if test -z $argv
-              echo "Usage: mkdirx folder"
-              return 1
-          end
+        mkdirx = {
+          description = "mkdir and cd";
+          body = ''
+            if test -z $argv
+                echo "Usage: mkdirx folder"
+                return 1
+            end
 
-          set folder $argv[1]
+            set folder $argv[1]
 
-          mkdir -p $folder
+            mkdir -p $folder
 
-          if test $status -ne 0
-              echo "Error: Failed to create directory $folder"
-              return $status
-          end
+            if test $status -ne 0
+                echo "Error: Failed to create directory $folder"
+                return $status
+            end
 
-          cd $folder
+            cd $folder
 
-          if test $status -ne 0
-              echo "Error: Failed to change to directory $folder"
-              return $status
-          end
-        '';
-        touchx = ''
-          if test -z $argv
-              echo "Usage: touchx file"
-              return 1
-          end
+            if test $status -ne 0
+                echo "Error: Failed to change to directory $folder"
+                return $status
+            end
+          '';
+        };
+        touchx = {
+          description = "touch and open in editor";
+          body = ''
+            if test -z $argv
+                echo "Usage: touchx file"
+                return 1
+            end
 
-          if not set -q EDITOR
-              echo "Error: $EDITOR is not set. Please configure your preferred editor using 'set -Ux EDITOR your-editor'"
-              return 1
-          end
+            if not set -q EDITOR
+                echo "Error: $EDITOR is not set. Please configure your preferred editor using 'set -Ux EDITOR your-editor'"
+                return 1
+            end
 
-          if not command -q $EDITOR
-              echo "Error: Editor '$EDITOR' not found. Please make sure it is installed and in your PATH."
-              return 1
-          end
+            if not command -q $EDITOR
+                echo "Error: Editor '$EDITOR' not found. Please make sure it is installed and in your PATH."
+                return 1
+            end
 
-          touch $argv[1]
+            touch $argv[1]
 
-          $EDITOR $argv[1]
-        '';
-        gclx = ''
-          # Check if an argument is provided
-          if test -z $argv
-              echo "Usage: gclx owner/repo or gclx https://github.com/owner/repo.git"
-              return 1
-          end
+            $EDITOR $argv[1]
+          '';
 
-          # Extract owner and repo names from the argument
-          set owner (echo $argv[1] | sed 's|^https://github.com/\(.*\)/\(.*\)\.git$|\1|')
-          set repo (echo $argv[1] | sed 's|^https://github.com/\(.*\)/\(.*\)\.git$|\2|')
+        };
+        gclx = {
+          description = "Git clone and cd";
+          body = ''
+            # Check if an argument is provided
+            if test -z $argv
+                echo "Usage: gclx owner/repo or gclx https://github.com/owner/repo.git"
+                return 1
+            end
 
-          # If the argument is not a full URL, assume it's in owner/repo format
-          if test "$owner" = "$argv[1]"
-              set owner (echo $argv[1] | cut -d '/' -f 1)
-              set repo (echo $argv[1] | cut -d '/' -f 2)
-          end
+            # Extract owner and repo names from the argument
+            set owner (echo $argv[1] | sed 's|^https://github.com/\(.*\)/\(.*\)\.git$|\1|')
+            set repo (echo $argv[1] | sed 's|^https://github.com/\(.*\)/\(.*\)\.git$|\2|')
 
-          # Perform git clone
-          if git clone "https://github.com/$owner/$repo.git" $repo
-              # Change to the newly cloned directory
-              cd $repo
-          else
-              echo "Error: Failed to clone repository."
-          end
-        '';
-        _fzf_dev_folder = ''
-          set -l selected_directory (ls ~/Dev | fzf --prompt="Directory> ")
+            # If the argument is not a full URL, assume it's in owner/repo format
+            if test "$owner" = "$argv[1]"
+                set owner (echo $argv[1] | cut -d '/' -f 1)
+                set repo (echo $argv[1] | cut -d '/' -f 2)
+            end
 
-          if test -n "$selected_directory"
-              # Change to the selected directory
-              cd ~/Dev/$selected_directory
-          end
+            # Perform git clone
+            if git clone "https://github.com/$owner/$repo.git" $repo
+                # Change to the newly cloned directory
+                cd $repo
+            else
+                echo "Error: Failed to clone repository."
+            end
+          '';
 
-          commandline --function repaint
-        '';
-      };
-      loginShellInit = ''
-        if which tmux >/dev/null 2>&1
-            if test "$TERM" != "screen-256color" -a "$TERM" != "screen"
-                # Get the list of tmux sessions
-                set sessions (tmux list-sessions 2> /dev/null)
-                if test -n "$sessions"
-                    # Check if "Hack" session exists in the list
-                    set hack_session (echo "$sessions" | grep -o 'Hack' | head -n 1)
-                    if test -n "$hack_session"
-                        # If "Hack" session exists, attach to it
-                        tmux attach-session -t "Hack"
+        };
+        _fzf_dev_folder = {
+          description = "fzf dev folder";
+          body = ''
+            set -l selected_directory (ls ~/Dev | fzf --prompt="Directory> ")
+
+            if test -n "$selected_directory"
+                # Change to the selected directory
+                cd ~/Dev/$selected_directory
+            end
+
+            commandline --function repaint
+          '';
+        };
+        __autotmux_hook = {
+          description = "Auto load tmux";
+          body = ''
+            if which tmux >/dev/null 2>&1
+                if test "$TERM" != "screen-256color" -a "$TERM" != "screen"
+                    # Get the list of tmux sessions
+                    set sessions (tmux list-sessions 2> /dev/null)
+                    if test -n "$sessions"
+                        # Check if "Hack" session exists in the list
+                        set hack_session (echo "$sessions" | grep -o 'Hack' | head -n 1)
+                        if test -n "$hack_session"
+                            # If "Hack" session exists, attach to it
+                            tmux attach-session -t "Hack"
+                        else
+                            # If "Hack" session not found, attach to the first one
+                            set first_session (echo "$sessions" | awk -F: '{print $1}' | head -n 1)
+                            tmux attach-session -t "$first_session"
+                        end
                     else
-                        # If "Hack" session not found, attach to the first one
-                        set first_session (echo "$sessions" | awk -F: '{print $1}' | head -n 1)
-                        tmux attach-session -t "$first_session"
+                        # If no sessions exist, create and attach to "Hack"
+                        tmux new-session -s Hack
                     end
-                else
-                    # If no sessions exist, create and attach to "Hack"
-                    tmux new-session -s Hack
                 end
             end
-        end
-
-        function __autols_hook --description "Auto ls" --on-event fish_prompt
-            if test -z $__autols_initialized
+          '';
+        };
+        __autols_hook = {
+          description = "Auto ls";
+          onVariable = "PWD";
+          body = ''
+            if not set -q __autols_initialized
                 set -g __autols_initialized 1
                 return
             end
 
-            if test "$__autols_last" != (pwd)
-                echo; ls
+            if test "$PWD" != "$__autols_last"
+                echo
+                ls
+                set -g __autols_last $PWD
             end
-            set -g __autols_last (pwd)
-        end
+          '';
+        };
+      };
+      loginShellInit = ''
+        __load-em
+        __autotmux_hook
+        __autols_hook
       '';
     };
   };
