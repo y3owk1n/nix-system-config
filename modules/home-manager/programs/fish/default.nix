020be15e4a575a57909f086d942e4a229f1c01d9
y3owk1n <wongyeowkin@gmail.com>
2024-01-09 16:56:31 +0800

    chore(fish): bump fish-x to 1.2.1

--- modules/home-manager/programs/fish/default.nix
+++ modules/home-manager/programs/fish/default.nix
@@ -76,8 +76,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "y3owk1n";
             repo = "fish-x";
-            rev = "v1.2.0";
-            sha256 = "sha256-T6L5mjAAsJvqXQifAI10rCkwI4vEbc6AmBcLvElmxgk=";
+            rev = "v1.2.1";
+            sha256 = "sha256-Hc6V1/ZPw/UiFds4pYn2U7bpXGAq4MN5h33K4cdOVbs=";
           };
         }
       ];
