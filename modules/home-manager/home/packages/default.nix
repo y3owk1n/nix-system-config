e1e6d46b004b6ba59a72ca0081c35ebcde2089da
y3owk1n <wongyeowkin@gmail.com>
2024-01-15 08:26:38 +0800

    chore: refactor neovim packages

--- modules/home-manager/home/packages/default.nix
+++ modules/home-manager/home/packages/default.nix
@@ -7,21 +7,22 @@
       bun
       fd
       stats
-      luajit
-      luajitPackages.luarocks # for nvim
-      nixfmt # for nvim
-      codespell # for nvim
-      beautysh # for nvim
       minio
       nodePackages.pnpm
       rm-improved
       fd
       curl
       less
-      neovim
-      pngpaste # only used for obsidian.nvim, not using it for now
       minio
       agkozak-zsh-prompt
+      # --- neovim ---
+      neovim
+      luajit
+      luajitPackages.luarocks # for nvim
+      nixfmt # for nvim
+      codespell # for nvim
+      beautysh # for nvim
+      pngpaste # only used for obsidian.nvim, not using it for now
     ];
   };
 }
