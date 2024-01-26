470b7957bb25a323b83d98a48dcf270cf8231663
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-01-26 18:38:22 +0800

    feat: add kanata to launchd daemons for auto load

--- modules/darwin/env/default.nix
+++ modules/darwin/env/default.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let kanata = import ../custom/kanata.nix;
+in {
   # Here go the darwin preferences and config items
   environment = {
-    systemPackages = with pkgs; [ coreutils cachix ];
+    systemPackages = with pkgs; [ coreutils cachix kanata ];
     shells = with pkgs; [ bash fish zsh ];
     loginShell = pkgs.fish;
     loginShellInit = ''
@@ -10,5 +12,22 @@
     systemPath =
       [ "/opt/homebrew/bin" "/opt/homebrew/sbin" "/run/current-system/sw/bin" ];
     pathsToLink = [ "/Applications" "/share/fish" ];
+    # launchDaemons.kanata = {
+    #   enable = true;
+    #   source = ../../home-manager/dotfiles/kanata/katana.plist;
+    # };
+  };
+  launchd.daemons.kanata = {
+    serviceConfig = {
+      KeepAlive = true;
+      RunAtLoad = true;
+      ProgramArguments = [
+        "/bin/sh"
+        "-c"
+        "exec sudo /run/current-system/sw/bin/kanata -n -c /Users/kylewong/.config/kanata/config.kbd"
+      ];
+      StandardErrorPath = "/var/log/kanata.log";
+      StandardOutPath = "/var/log/kanata.log";
+    };
   };
 }
