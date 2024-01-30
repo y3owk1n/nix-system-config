97db31f00a4d4cb2fd7a4865fc15d6c5834ac240
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-01-30 13:40:02 +0800

    feat(nvim): add kmonad-vim syntax highlighting

--- modules/home-manager/dotfiles/nvim/lua/k92/plugins/init.lua
+++ modules/home-manager/dotfiles/nvim/lua/k92/plugins/init.lua
@@ -3,6 +3,7 @@ return {
 	{ "MunifTanjim/nui.nvim", lazy = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{ "christoomey/vim-tmux-navigator" },
+	{ "kmonad/kmonad-vim", lazy = true },
 	{
 		"b0o/SchemaStore.nvim",
 		lazy = true,
