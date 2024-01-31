5c41a6eac96dbca11c459bd2c0b0f24dadeab980
y3owk1n <wongyeowkin@gmail.com>
2024-01-31 15:58:23 +0800

    feat(nvim): add gitsign preview hunk keymap

--- modules/home-manager/dotfiles/nvim/lua/k92/plugins/gitsigns.lua
+++ modules/home-manager/dotfiles/nvim/lua/k92/plugins/gitsigns.lua
@@ -1,6 +1,20 @@
+local keymap = require("k92.utils.keymaps")
+local map = keymap.map
+
 return {
 	"lewis6991/gitsigns.nvim",
 	lazy = true,
 	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-	config = true,
+	config = function(_, opts)
+		local gitsigns = require("gitsigns")
+
+		gitsigns.setup(opts)
+
+		map(
+			"n",
+			"<leader>gp",
+			":Gitsigns preview_hunk<cr>",
+			{ desc = "Preview hunk" }
+		)
+	end,
 }
