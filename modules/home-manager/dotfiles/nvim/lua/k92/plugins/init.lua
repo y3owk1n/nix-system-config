161229e4453706eeeb525d56c62524e46dd43f20
y3owk1n <wongyeowkin@gmail.com>
2024-02-01 10:32:42 +0800

    feat(nvim): remove unused plugins

--- modules/home-manager/dotfiles/nvim/lua/k92/plugins/init.lua
+++ modules/home-manager/dotfiles/nvim/lua/k92/plugins/init.lua
@@ -1,9 +1,7 @@
 return {
 	{ "nvim-lua/plenary.nvim", lazy = true },
-	{ "MunifTanjim/nui.nvim", lazy = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{ "christoomey/vim-tmux-navigator" },
-	{ "kmonad/kmonad-vim", lazy = true },
 	{
 		"b0o/SchemaStore.nvim",
 		lazy = true,
