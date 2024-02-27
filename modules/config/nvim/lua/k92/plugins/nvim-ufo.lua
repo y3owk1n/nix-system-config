0653e9c058546d6ddfd589068af9ca7fade6ffa0
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-02-27 18:57:54 +0800

    feat(nvim): temporary remove ufo, something is wrong

--- modules/config/nvim/lua/k92/plugins/nvim-ufo.lua
+++ modules/config/nvim/lua/k92/plugins/nvim-ufo.lua
@@ -3,26 +3,26 @@ local map = keymap.map
 
 return {
 	{
-		"kevinhwang91/nvim-ufo",
-		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-		dependencies = {
-			"kevinhwang91/promise-async",
-		},
-		config = function()
-			local ufo = require("ufo")
-			ufo.setup({
-				provider_selector = function(_bufnr, _filetype, _buftype)
-					return { "treesitter", "indent" }
-				end,
-			})
-
-			map("n", "zR", function()
-				ufo.openAllFolds()
-			end, { desc = "Open all folds" })
-
-			map("n", "zM", function()
-				ufo.closeAllFolds()
-			end, { desc = "Close all folds" })
-		end,
+		-- "kevinhwang91/nvim-ufo",
+		-- event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+		-- dependencies = {
+		-- 	"kevinhwang91/promise-async",
+		-- },
+		-- config = function()
+		-- 	local ufo = require("ufo")
+		-- 	ufo.setup({
+		-- 		provider_selector = function(_bufnr, _filetype, _buftype)
+		-- 			return { "treesitter", "indent" }
+		-- 		end,
+		-- 	})
+		--
+		-- 	map("n", "zR", function()
+		-- 		ufo.openAllFolds()
+		-- 	end, { desc = "Open all folds" })
+		--
+		-- 	map("n", "zM", function()
+		-- 		ufo.closeAllFolds()
+		-- 	end, { desc = "Close all folds" })
+		-- end,
 	},
 }
