6c74bf3502c532622c1e9a9f8c47a493dcc5dda3
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-02-29 17:39:42 +0800

    feat(brew): add apps for mas

--- modules/darwin/homebrew/mas-apps.nix
+++ modules/darwin/homebrew/mas-apps.nix
@@ -1,1 +1,11 @@
-{ ... }: { homebrew = { masApps = { }; }; }
+{ ... }: {
+  homebrew = {
+    masApps = {
+      "prettyJSON for Safari" = 1445328303;
+      "Wappalyzer" = 1520333300;
+      "Vimari" = 1480933944;
+      "Super Agent for Safari" = 1568262835;
+      "AdGuard for Safari" = 1440147259;
+    };
+  };
+}
