5165f4db49d941a733e272da9de5a31cb479ad64
y3owk1n <wongyeowkin@gmail.com>
2024-03-04 08:30:27 +0800

    feat(nvim): update oil.lsp_rename_autosave

--- modules/config/nvim/lua/k92/plugins/oil.lua
+++ modules/config/nvim/lua/k92/plugins/oil.lua
@@ -19,7 +19,13 @@ return {
 				view_options = {
 					show_hidden = true,
 				},
-				lsp_rename_autosave = true,
+				lsp_file_methods = {
+					-- Time to wait for LSP file operations to complete before skipping
+					timeout_ms = 1000,
+					-- Set to true to autosave buffers that are updated with LSP willRenameFiles
+					-- Set to "unmodified" to only save unmodified buffers
+					autosave_changes = false,
+				},
 				delete_to_trash = true,
 				skip_confirm_for_simple_edits = true,
 				keymaps = {
