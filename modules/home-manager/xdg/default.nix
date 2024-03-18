8e3f32bd98e92f3a0a27281200b463c751b06801
y3owk1n <wongyeowkin@gmail.com>
2024-03-18 23:01:51 +0800

    feat: remove unused casks and configs

--- modules/home-manager/xdg/default.nix
+++ modules/home-manager/xdg/default.nix
@@ -5,11 +5,6 @@
         "${config.home.homeDirectory}/nix-system-config/modules/config/nvim";
       recursive = true;
     };
-    karabiner = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/nix-system-config/modules/config/karabiner";
-      recursive = true;
-    };
     kanata = {
       source = config.lib.file.mkOutOfStoreSymlink
         "${config.home.homeDirectory}/nix-system-config/modules/config/kanata";
