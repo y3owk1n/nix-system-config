134fe252fd9d709723da169549e0ee95df1f77b5
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-03-27 13:07:27 +0800

    feat: remove kanata and surfingkeys from xdg config

--- modules/home-manager/xdg/default.nix
+++ modules/home-manager/xdg/default.nix
@@ -5,16 +5,16 @@
         "${config.home.homeDirectory}/nix-system-config/modules/config/nvim";
       recursive = true;
     };
-    kanata = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/nix-system-config/modules/config/kanata";
-      recursive = true;
-    };
-    surfingkeys = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/nix-system-config/modules/config/surfingkeys";
-      recursive = true;
-    };
+    # kanata = {
+    #   source = config.lib.file.mkOutOfStoreSymlink
+    #     "${config.home.homeDirectory}/nix-system-config/modules/config/kanata";
+    #   recursive = true;
+    # };
+    # surfingkeys = {
+    #   source = config.lib.file.mkOutOfStoreSymlink
+    #     "${config.home.homeDirectory}/nix-system-config/modules/config/surfingkeys";
+    #   recursive = true;
+    # };
     svim = {
       source = config.lib.file.mkOutOfStoreSymlink
         "${config.home.homeDirectory}/nix-system-config/modules/config/svim";
