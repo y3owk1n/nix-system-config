12a29f4064a1ca5efb1e312368ab7c6ec8631c82
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-04-18 16:08:34 +0800

    feat: Add Vimlike extension and update mappings
    Add the "Vimlike" extension to the list of MAS apps in the Nix configuration file.
    Update the settings.txt file for the Vimlike extension with new key mappings for scrolling, navigation, tab management, video control, dark mode, reload, help, and URL actions.

--- modules/darwin/homebrew/mas-apps.nix
+++ modules/darwin/homebrew/mas-apps.nix
@@ -3,10 +3,11 @@
   homebrew = {
     masApps = {
       "Wappalyzer" = 1520333300;
-      "Vimari" = 1480933944;
+      # "Vimari" = 1480933944;
       "AdGuard for Safari" = 1440147259;
       "Consent-O-Matic" = 1606897889;
       "JSON Peep for Safari" = 1458969831;
+      "Vimlike" = 1584519802;
     };
   };
 }
