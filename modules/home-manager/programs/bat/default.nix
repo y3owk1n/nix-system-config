65e72e17cab0925850b393aad6ee71b89760af8d
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-04-19 12:02:51 +0800

    build: Update bat theme to latest version
    Changed the revision and sha256 of the bat theme to match the latest version.

--- modules/home-manager/programs/bat/default.nix
+++ modules/home-manager/programs/bat/default.nix
@@ -15,8 +15,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "catppuccin";
             repo = "bat"; # Bat uses sublime syntax for its themes
-            rev = "b19bea35a85a32294ac4732cad5b0dc6495bed32";
-            sha256 = "sha256-POoW2sEM6jiymbb+W/9DKIjDM1Buu1HAmrNP0yC2JPg=";
+            rev = "b8134f01b0ac176f1cf2a7043a5abf5a1a29457b";
+            sha256 = "sha256-gzf0/Ltw8mGMsEFBTUuN33MSFtUP4xhdxfoZFntaycQ=";
           };
           file = "themes/Catppuccin Macchiato.tmTheme";
         };
