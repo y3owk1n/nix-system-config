7cfc477085484cb5aa8322af83b79d79731ad38a
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-04-20 17:04:49 +0800

    feat: Add harpoon navigation index component to neo-tree (#37)
    Add harpoon navigation index component to neo-tree renderer to visually
    indicate the position of current file in harpoon navigation list. This
    enhances navigation experience within the tree.

--- modules/config/nvim/lua/k92/plugins/neo-tree.lua
+++ modules/config/nvim/lua/k92/plugins/neo-tree.lua
@@ -54,8 +54,41 @@ return {
 		},
 		filesystem = {
 			bind_to_cwd = false,
-			follow_current_file = { enabled = true },
+			follow_current_file = { enabled = true, leave_dirs_open = true },
 			use_libuv_file_watcher = true,
+			components = {
+				harpoon_index = function(config, node, _)
+					local harpoon_list = require("harpoon"):list()
+					local path = node:get_id()
+					local harpoon_key = vim.uv.cwd()
+
+					for i, item in ipairs(harpoon_list.items) do
+						local value = item.value
+						if string.sub(item.value, 1, 1) ~= "/" then
+							value = harpoon_key .. "/" .. item.value
+						end
+
+						if value == path then
+							vim.print(path)
+							return {
+								text = string.format(" ⥤ %d", i), -- <-- Add your favorite harpoon like arrow here
+								highlight = config.highlight
+									or "NeoTreeDirectoryIcon",
+							}
+						end
+					end
+					return {}
+				end,
+			},
+			renderers = {
+				file = {
+					{ "icon" },
+					{ "name", use_git_status_colors = true },
+					{ "harpoon_index" }, --> This is what actually adds the component in where you want it
+					{ "diagnostics" },
+					{ "git_status", highlight = "NeoTreeDimText" },
+				},
+			},
 		},
 		window = {
 			position = "current",
