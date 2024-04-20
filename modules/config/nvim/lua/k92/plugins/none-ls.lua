ab83e8acbea14f271b867d40f85e9a61cdf21ace
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-04-21 01:01:19 +0800

    refactor: neovim config (#47)

--- modules/config/nvim/lua/k92/plugins/none-ls.lua
+++ modules/config/nvim/lua/k92/plugins/none-ls.lua
@@ -6,6 +6,7 @@ return {
 	},
 	opts = function(_, opts)
 		local nls = require("null-ls")
+
 		opts.root_dir = opts.root_dir
 			or require("null-ls.utils").root_pattern(
 				".null-ls-root",
@@ -13,33 +14,27 @@ return {
 				"Makefile",
 				".git"
 			)
+
+		local eslint_configs_files = {
+			".eslintrc.js",
+			".eslintrc.cjs",
+			".eslintrc.yaml",
+			".eslintrc.yml",
+			".eslintrc.json",
+			"eslint.config.js",
+			"eslint.config.mjs",
+			"eslint.config.cjs",
+		}
+
 		opts.sources = vim.list_extend(opts.sources or {}, {
 			require("none-ls.code_actions.eslint_d").with({
 				condition = function(utils)
-					return utils.root_has_file({
-						".eslintrc.js",
-						".eslintrc.cjs",
-						".eslintrc.yaml",
-						".eslintrc.yml",
-						".eslintrc.json",
-						"eslint.config.js",
-						"eslint.config.mjs",
-						"eslint.config.cjs",
-					})
+					return utils.root_has_file(eslint_configs_files)
 				end,
 			}),
 			require("none-ls.diagnostics.eslint_d").with({
 				condition = function(utils)
-					return utils.root_has_file({
-						".eslintrc.js",
-						".eslintrc.cjs",
-						".eslintrc.yaml",
-						".eslintrc.yml",
-						".eslintrc.json",
-						"eslint.config.js",
-						"eslint.config.mjs",
-						"eslint.config.cjs",
-					})
+					return utils.root_has_file(eslint_configs_files)
 				end,
 			}),
 			require("none-ls.diagnostics.yamllint"),
--- modules/config/nvim/lua/k92/plugins/todo-comment.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"folke/todo-comments.nvim",
-	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = { signs = false },
-}
