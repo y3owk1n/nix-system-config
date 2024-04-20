210bd33e24d2a3c94487c611daaed33396f94b47
y3owk1n <wongyeowkin@gmail.com>
2024-04-20 22:33:58 +0800

    feat: Add null-ls configuration for eslint and yamllint
    Add configuration for eslint code actions and diagnostics as well as yamllint diagnostics to enhance linting capabilities in the project.

---
+++ modules/config/nvim/lua/k92/plugins/none-ls.lua
@@ -0,0 +1,50 @@
+return {
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+		"mason.nvim",
+	},
+	opts = function(_, opts)
+		local nls = require("null-ls")
+		opts.root_dir = opts.root_dir
+			or require("null-ls.utils").root_pattern(
+				".null-ls-root",
+				".neoconf.json",
+				"Makefile",
+				".git"
+			)
+		opts.sources = vim.list_extend(opts.sources or {}, {
+			require("none-ls.code_actions.eslint_d").with({
+				condition = function(utils)
+					return utils.root_has_file({
+						".eslintrc.js",
+						".eslintrc.cjs",
+						".eslintrc.yaml",
+						".eslintrc.yml",
+						".eslintrc.json",
+						"eslint.config.js",
+						"eslint.config.mjs",
+						"eslint.config.cjs",
+					})
+				end,
+			}),
+			require("none-ls.diagnostics.eslint_d").with({
+				condition = function(utils)
+					return utils.root_has_file({
+						".eslintrc.js",
+						".eslintrc.cjs",
+						".eslintrc.yaml",
+						".eslintrc.yml",
+						".eslintrc.json",
+						"eslint.config.js",
+						"eslint.config.mjs",
+						"eslint.config.cjs",
+					})
+				end,
+			}),
+			require("none-ls.diagnostics.yamllint"),
+			nls.builtins.diagnostics.fish,
+			nls.builtins.diagnostics.markdownlint,
+		})
+	end,
+}
