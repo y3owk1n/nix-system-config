0a90a99a57000e618ab5a89e15cef474a8a2db78
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-04-20 15:02:55 +0800

    feat: switch to neo-tree from oil (#33)
    oil keep crashing with scheme not found, come back later when its ok

--- modules/config/nvim/lua/k92/plugins/oil.lua
+++ modules/config/nvim/lua/k92/plugins/oil.lua
@@ -1,61 +1,52 @@
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "oil",
-	callback = function()
-		vim.opt_local.colorcolumn = ""
-	end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "oil",
+-- 	callback = function()
+-- 		vim.opt_local.colorcolumn = ""
+-- 	end,
+-- })
 
 return {
-	{
-		"stevearc/oil.nvim",
-		opts = {
-			view_options = {
-				show_hidden = true,
-			},
-			lsp_file_methods = {
-				-- Time to wait for LSP file operations to complete before skipping
-				timeout_ms = 1000,
-				-- Set to true to autosave buffers that are updated with LSP willRenameFiles
-				-- Set to "unmodified" to only save unmodified buffers
-				autosave_changes = false,
-			},
-			delete_to_trash = true,
-			skip_confirm_for_simple_edits = true,
-			keymaps = {
-				["?"] = "actions.show_help",
-				["<CR>"] = "actions.select",
-				["<leader>o\\"] = "actions.select_vsplit",
-				["<leader>o-"] = "actions.select_split", -- this is used to navigate left
-				["<leader>or"] = "actions.refresh",
-				["<leader>os"] = "actions.change_sort",
-				["<leader>ox"] = "actions.open_external",
-				["<leader>oh"] = "actions.toggle_hidden",
-				["<leader>ot"] = "actions.toggle_trash",
-				["-"] = "actions.parent",
-				["_"] = "actions.open_cwd",
-			},
-			use_default_keymaps = false,
-			experimental_watch_for_changes = true,
-		},
-		-- Optional dependencies
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {
-			{
-				"<leader>e",
-				mode = { "n" },
-				function()
-					require("oil").toggle_float()
-				end,
-				desc = "[E]xplorer",
-			},
-			{
-				"<leader>E",
-				mode = { "n" },
-				function()
-					require("oil").toggle_float(vim.fn.getcwd())
-				end,
-				desc = "[E]xplorer (CWD)",
-			},
-		},
-	},
+	-- "stevearc/oil.nvim",
+	-- opts = {
+	-- 	view_options = {
+	-- 		show_hidden = true,
+	-- 	},
+	-- 	delete_to_trash = true,
+	-- 	skip_confirm_for_simple_edits = true,
+	-- 	keymaps = {
+	-- 		["?"] = "actions.show_help",
+	-- 		["<CR>"] = "actions.select",
+	-- 		["<leader>o\\"] = "actions.select_vsplit",
+	-- 		["<leader>o-"] = "actions.select_split", -- this is used to navigate left
+	-- 		["<leader>or"] = "actions.refresh",
+	-- 		["<leader>os"] = "actions.change_sort",
+	-- 		["<leader>ox"] = "actions.open_external",
+	-- 		["<leader>oh"] = "actions.toggle_hidden",
+	-- 		["<leader>ot"] = "actions.toggle_trash",
+	-- 		["-"] = "actions.parent",
+	-- 		["_"] = "actions.open_cwd",
+	-- 	},
+	-- 	use_default_keymaps = false,
+	-- 	experimental_watch_for_changes = true,
+	-- },
+	-- -- Optional dependencies
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- keys = {
+	-- 	{
+	-- 		"<leader>e",
+	-- 		mode = { "n" },
+	-- 		function()
+	-- 			require("oil").toggle_float()
+	-- 		end,
+	-- 		desc = "[E]xplorer",
+	-- 	},
+	-- 	{
+	-- 		"<leader>E",
+	-- 		mode = { "n" },
+	-- 		function()
+	-- 			require("oil").toggle_float(vim.fn.getcwd())
+	-- 		end,
+	-- 		desc = "[E]xplorer (CWD)",
+	-- 	},
+	-- },
 }
