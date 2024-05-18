d9a0b0cc121328a555b1b796753971d7b8aca9aa
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-05-18 16:43:37 +0800

    fix: pin indent-blankline to specific commit hash (#104)

--- modules/config/nvim/lua/k92/plugins/init.lua
+++ modules/config/nvim/lua/k92/plugins/init.lua
@@ -32,6 +32,7 @@ return {
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		commit = "29be0919b91fb59eca9e90690d76014233392bef",
 		lazy = true,
 		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 		main = "ibl",
