c9ec12de899558138b055a2d1d5f5bf5e5edf95c
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-05-30 18:37:58 +0800

    feat: bump catppuccin custom deps

--- modules/home-manager/programs/bat/default.nix
+++ modules/home-manager/programs/bat/default.nix
@@ -15,8 +15,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "catppuccin";
             repo = "bat"; # Bat uses sublime syntax for its themes
-            rev = "b8134f01b0ac176f1cf2a7043a5abf5a1a29457b";
-            sha256 = "sha256-gzf0/Ltw8mGMsEFBTUuN33MSFtUP4xhdxfoZFntaycQ=";
+            rev = "d714cc1d358ea51bfc02550dabab693f70cccea0";
+            sha256 = "sha256-Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
           };
           file = "themes/Catppuccin Macchiato.tmTheme";
         };
