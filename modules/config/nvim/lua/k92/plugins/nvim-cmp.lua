3d9225011e8b15d5e155fbb16d670b457a7a6210
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-05-30 14:12:48 +0800

    feat: add luasnip make (#130)

--- modules/config/nvim/lua/k92/plugins/nvim-cmp.lua
+++ modules/config/nvim/lua/k92/plugins/nvim-cmp.lua
@@ -7,7 +7,7 @@ return {
 		{
 			"L3MON4D3/LuaSnip",
 			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- build = "make install_jsregexp",
+			build = "make install_jsregexp",
 		},
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"hrsh7th/cmp-nvim-lsp",
