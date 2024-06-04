a5fca26de970a6f176a07d34016910c98cd7bb20
y3owk1n <wongyeowkin@gmail.com>
2024-06-04 09:28:48 +0800

    feat: update only affected lines when sort

--- modules/config/nvim/lua/k92/core/usercmds.lua
+++ modules/config/nvim/lua/k92/core/usercmds.lua
@@ -4,7 +4,14 @@ local usercmd = vim.api.nvim_create_user_command
 
 usercmd("TailwindSortRun", function()
 	local tailwind_sort = require("k92.utils.tailwind-sort")
-	tailwind_sort.run()
+
+	-- No need to check for autorun flag & prettier tw plugin existence
+	-- This is on demand run
+	-- Tailwind LSP will be checked in the run function
+
+	vim.schedule(function() -- Schedule to run after current operations complete
+		tailwind_sort.run()
+	end)
 end, {})
 
 usercmd("TailwindSortStatus", function()
@@ -13,22 +20,22 @@ usercmd("TailwindSortStatus", function()
 		"TailwindSort Status: " .. vim.inspect(tailwind_sort.get_status()),
 		vim.log.levels.INFO
 	)
-end, {})
+end, {
+	desc = "Check TailwindSort auto format status",
+})
 
 usercmd("TailwindSortEnable", function()
 	local tailwind_sort = require("k92.utils.tailwind-sort")
-	local status = tailwind_sort.toggle_status(true)
-	vim.notify(
-		"TailwindSort Status: " .. vim.inspect(status),
-		vim.log.levels.INFO
-	)
-end, {})
+	tailwind_sort.toggle_status(true)
+	vim.notify("Enabled TailwindSort auto format on save")
+end, {
+	desc = "Enable TailwindSort auto format on save",
+})
 
 usercmd("TailwindSortDisable", function()
 	local tailwind_sort = require("k92.utils.tailwind-sort")
-	local status = tailwind_sort.toggle_status(false)
-	vim.notify(
-		"TailwindSort Status: " .. vim.inspect(status),
-		vim.log.levels.INFO
-	)
-end, {})
+	tailwind_sort.toggle_status(false)
+	vim.notify("Disabled TailwindSort auto format on save")
+end, {
+	desc = "Disable TailwindSort auto format on save",
+})
