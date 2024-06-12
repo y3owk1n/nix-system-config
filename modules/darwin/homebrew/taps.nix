dcdb460c56b2896f6ef2c4c0474c82f6d331f504
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-06-12 17:32:36 +0800

    feat: remove deprecated cask

--- modules/darwin/homebrew/taps.nix
+++ modules/darwin/homebrew/taps.nix
@@ -3,7 +3,7 @@
   homebrew = {
     taps = [
       "homebrew/services"
-      "homebrew/cask-fonts"
+      # "homebrew/cask-fonts"
       # "homebrew/cask-versions"
       "FelixKratz/formulae"
     ];
