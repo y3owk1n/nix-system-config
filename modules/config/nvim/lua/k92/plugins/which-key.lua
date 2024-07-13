ae1a43d983bd242f39fccb4674e0f796146f0c98
y3owk1n <wongyeowkin@gmail.com>
2024-07-13 16:34:33 +0800

    feat: update which key with new settings

--- modules/config/nvim/lua/k92/plugins/which-key.lua
+++ modules/config/nvim/lua/k92/plugins/which-key.lua
@@ -8,30 +8,46 @@ return {
 		vim.o.timeoutlen = 500
 	end,
 	opts = {
-		plugins = { spelling = true },
-		window = {
+		preset = "modern",
+		win = {
 			border = "rounded", -- none, single, double, shadow
 		},
-		defaults = {
-			mode = { "n", "v" },
-			["g"] = { name = "+goto" },
-			["gs"] = { name = "+surround" },
-			["]"] = { name = "+next" },
-			["["] = { name = "+prev" },
-			["<leader>c"] = { name = "+[c]ode" },
-			["<leader>l"] = { name = "+[l]sp" },
-			["<leader>lb"] = { name = "+[l]sp [b]uiltin" },
-			["<leader>f"] = { name = "+[f]ile" },
-			["<leader>g"] = { name = "+[g]it" },
-			["<leader>h"] = { name = "+[h]arpoon" },
-			["<leader>s"] = { name = "+[s]earch" },
-			["<leader>w"] = { name = "+[w]indows" },
-			["<leader>x"] = { name = "+[t]rouble" },
+		spec = {
+			{
+				-- Nested mappings are allowed and can be added in any order
+				-- Most attributes can be inherited or overridden on any level
+				-- There's no limit to the depth of nesting
+				mode = { "n", "v" }, -- NORMAL and VISUAL mode
+				{ "g", desc = "+goto" }, -- no need to specify mode since it's inherited
+				{ "gs", desc = "+surround" }, -- no need to specify mode since it's inherited
+				{ "]", desc = "+next" }, -- no need to specify mode since it's inherited
+				{ "[", desc = "+prev" }, -- no need to specify mode since it's inherited
+				{ "<leader>c", desc = "+[c]ode" }, -- no need to specify mode since it's inherited
+				{ "<leader>l", desc = "+[l]sp" }, -- no need to specify mode since it's inherited
+				{ "<leader>lb", desc = "+[l]sp [b]uiltin" }, -- no need to specify mode since it's inherited
+				{ "<leader>f", desc = "+[f]ile" }, -- no need to specify mode since it's inherited
+				{ "<leader>g", desc = "+[g]it" }, -- no need to specify mode since it's inherited
+				{ "<leader>h", desc = "+[h]arpoon" }, -- no need to specify mode since it's inherited
+				{ "<leader>s", desc = "+[s]earch" }, -- no need to specify mode since it's inherited
+				{ "<leader>w", desc = "+[w]indows" }, -- no need to specify mode since it's inherited
+				{ "<leader>x", desc = "+[t]rouble" }, -- no need to specify mode since it's inherited
+			},
 		},
+		-- defaults = {
+		-- 	mode = { "n", "v" },
+		-- 	["g"] = { name = "+goto" },
+		-- 	["gs"] = { name = "+surround" },
+		-- 	["]"] = { name = "+next" },
+		-- 	["["] = { name = "+prev" },
+		-- 	["<leader>c"] = { name = "+[c]ode" },
+		-- 	["<leader>l"] = { name = "+[l]sp" },
+		-- 	["<leader>lb"] = { name = "+[l]sp [b]uiltin" },
+		-- 	["<leader>f"] = { name = "+[f]ile" },
+		-- 	["<leader>g"] = { name = "+[g]it" },
+		-- 	["<leader>h"] = { name = "+[h]arpoon" },
+		-- 	["<leader>s"] = { name = "+[s]earch" },
+		-- 	["<leader>w"] = { name = "+[w]indows" },
+		-- 	["<leader>x"] = { name = "+[t]rouble" },
+		-- },
 	},
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-		wk.register(opts.defaults)
-	end,
 }
