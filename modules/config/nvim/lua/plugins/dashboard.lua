c35518e0d7a1724eb04af0bac8a4b776b9dbaba2
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-07-14 22:01:08 +0800

    feat: update dashboard (#190)

---
+++ modules/config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,30 @@
+return {
+  "nvimdev/dashboard-nvim",
+  opts = function(_, opts)
+    local logo = [[
+██╗  ██╗██╗   ██╗██╗     ███████╗
+██║ ██╔╝╚██╗ ██╔╝██║     ██╔════╝
+█████╔╝  ╚████╔╝ ██║     █████╗  
+██╔═██╗   ╚██╔╝  ██║     ██╔══╝  
+██║  ██╗   ██║   ███████╗███████╗
+╚═╝  ╚═╝   ╚═╝   ╚══════╝╚══════╝
+    ]]
+
+    logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+    opts.config.header = vim.split(logo, "\n")
+
+    -- stylua: ignore
+    opts.config.center = {
+        { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
+        { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
+        { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+        { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
+        { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+        { action = "Mason",                                          desc = " Mason",           icon = "󰛦 ", key = "m" },
+        { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+        { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+        { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+    }
+  end,
+}
