e344e79f793c8411a7659e6c11c3ae1d565b7e07
y3owk1n <wongyeowkin@gmail.com>
2024-07-15 00:16:30 +0800

    feat: add diagnostic border

--- modules/config/nvim/lua/plugins/nvim-lspconfig.lua
+++ modules/config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
   -- other settings removed for brevity
+  ---@class PluginLspOpts
   opts = {
-    ---@type lspconfig.options
     servers = {
       biome = {},
     },
