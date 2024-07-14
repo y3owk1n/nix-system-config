9b9d8b076d6a8f3ad5a8050d1839a4f1308deaa7
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-07-14 17:19:33 +0800

    feat: make sure biome and prettier wont clash (#186)

--- modules/config/nvim/lua/plugins/conform.lua
+++ modules/config/nvim/lua/plugins/conform.lua
@@ -10,6 +10,27 @@ return {
           return find_root(ctx, { "biome.json" })
         end,
       },
+      prettier = {
+        ---@diagnostic disable-next-line: unused-local
+        condition = function(self, ctx)
+          local prettier_configs = {
+            ".prettierrc",
+            ".prettierrc.json",
+            ".prettierrc.yml",
+            ".prettierrc.yaml",
+            ".prettierrc.json5",
+            ".prettierrc.js",
+            "prettier.config.js",
+            ".prettierrc.mjs",
+            "prettier.config.mjs",
+            ".prettierrc.cjs",
+            "prettier.config.cjs",
+            ".prettierrc.toml",
+          }
+
+          return find_root(ctx, prettier_configs)
+        end,
+      },
     },
     formatters_by_ft = {
       javascript = { { "biome", "prettier" } },
