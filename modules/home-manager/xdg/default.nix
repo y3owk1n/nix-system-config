42ee0f6a97764c3b5f8983b28674c6a4c916d5f8
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-07-23 17:17:26 +0800

    feat: remove svim (#202)

--- modules/home-manager/xdg/default.nix
+++ modules/home-manager/xdg/default.nix
@@ -15,10 +15,10 @@
     #     "${config.home.homeDirectory}/nix-system-config/modules/config/surfingkeys";
     #   recursive = true;
     # };
-    svim = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-system-config/modules/config/svim";
-      recursive = true;
-    };
+    # svim = {
+    #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-system-config/modules/config/svim";
+    #   recursive = true;
+    # };
     # tridactyl = {
     #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-system-config/modules/config/tridactyl";
     #   recursive = true;
