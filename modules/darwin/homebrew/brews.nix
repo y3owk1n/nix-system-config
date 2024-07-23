b24c22937542bcc508de699c42389ddadf0ade1a
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-07-23 17:17:03 +0800

    feat: remove svim

--- modules/darwin/homebrew/brews.nix
+++ modules/darwin/homebrew/brews.nix
@@ -5,11 +5,11 @@
       "sqlite"
       # "vercel-cli" # install it from nix
       "gnu-sed"
-      {
-        name = "svim";
-        start_service = true;
-        restart_service = "changed";
-      }
+      # {
+      #   name = "svim";
+      #   start_service = true;
+      #   restart_service = "changed";
+      # }
     ];
   };
 }
