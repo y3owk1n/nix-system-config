c97357ef3b013d3f2afe2fbf2c18714e4e722aa8
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-07-29 15:36:24 +0800

    feat(wezterm): refactor wezterm config (#222)

--- modules/config/wezterm/wezterm.lua
+++ modules/config/wezterm/wezterm.lua
@@ -1,26 +1,50 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+-- color scheme
 config.color_scheme = "Catppuccin Macchiato"
+
+-- font
 config.font = wezterm.font("GeistMono NF", { weight = "Bold" })
 config.font_size = 14
-config.max_fps = 240
+
+-- UI
 config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
 config.window_decorations = "RESIZE"
 config.audible_bell = "Disabled"
-config.disable_default_key_bindings = true
 
+-- fps
+config.max_fps = 240
+config.animation_fps = 240
+
+-- unused features
+config.mouse_wheel_scrolls_tabs = false
+
+-- reset key bindings
+config.disable_default_key_bindings = true
+config.disable_default_quick_select_patterns = false
+
+-- key bindings
 config.keys = {
 	{
 		key = "q",
 		mods = "CMD",
 		action = wezterm.action.QuitApplication,
+	},
+	{
+		key = "c",
+		mods = "CMD",
+		action = wezterm.action.CopyTo("Clipboard"),
+	},
+	{
+		key = "v",
+		mods = "CMD",
+		action = wezterm.action.PasteFrom("Clipboard"),
 	},
 	{
 		key = "w",
@@ -29,6 +53,8 @@ config.keys = {
 	},
 }
 
+-- others
+config.quit_when_all_windows_are_closed = true
 config.skip_close_confirmation_for_processes_named = {
 	"bash",
 	"sh",
@@ -37,8 +63,6 @@ config.skip_close_confirmation_for_processes_named = {
 	"tmux",
 	"zellij",
 }
-
 config.window_close_confirmation = "NeverPrompt"
 
--- and finally, return the configuration to wezterm
 return config
