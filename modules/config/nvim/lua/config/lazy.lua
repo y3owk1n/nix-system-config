6b4677b3679dc62072f23c70bb2f9f1c03033f8d
y3owk1n <wongyeowkin@gmail.com>
2024-08-03 11:22:24 +0800

    feat(nvim): add toml as extras in lazyvim

--- modules/config/nvim/lua/config/lazy.lua
+++ modules/config/nvim/lua/config/lazy.lua
@@ -51,6 +51,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
+		{ import = "lazyvim.plugins.extras.lang.toml" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
