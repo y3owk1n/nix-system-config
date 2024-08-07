17529094e8d1432b604fadd58278ac86c03d068f
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-08-07 17:01:59 +0800

    feat(mas): add safari plugins

--- modules/darwin/homebrew/mas-apps.nix
+++ modules/darwin/homebrew/mas-apps.nix
@@ -3,11 +3,11 @@
   homebrew = {
     masApps = {
       # --- when want to use safari ---
-      # "Wappalyzer" = 1520333300;
-      # "AdGuard for Safari" = 1440147259;
-      # "Consent-O-Matic" = 1606897889;
-      # "JSON Peep for Safari" = 1458969831;
-      # "Vimlike" = 1584519802;
+      "Wappalyzer" = 1520333300;
+      "AdGuard for Safari" = 1440147259;
+      "Consent-O-Matic" = 1606897889;
+      "JSON Peep for Safari" = 1458969831;
+      "Vimlike" = 1584519802;
 
       # --- misc ---
       "Tailscale" = 1475387142;
