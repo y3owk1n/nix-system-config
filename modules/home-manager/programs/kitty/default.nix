51fe8e1fcbbab0bc68e2fbb03ba4f332ab335f45
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-08-11 02:05:43 +0800

    feat(kitty): add conf to nix directly (#287)

--- modules/home-manager/programs/kitty/default.nix
+++ modules/home-manager/programs/kitty/default.nix
@@ -11,13 +11,22 @@
       settings = {
         scrollback_lines = 10000;
         enable_audio_bell = false;
-        font_family = "GeistMono NF";
+        font_family = "JetBrainsMono Nerd Font Mono Regular";
         bold_font = "auto";
         italic_font = "auto";
         bold_italic_font = "auto";
-        font_size = 14;
+        font_size = 13;
         window_padding_width = 10;
         hide_window_decorations = "titlebar-only";
+        tab_bar_style = "hidden";
+        clear_all_shortcuts = "yes";
+        clear_all_mouse_actions = "no";
+      };
+      keybindings = {
+        "cmd+w" = "close_window";
+        "cmd+n" = "new_window";
+        "cmd+t" = "new_window";
+        "cmd+q" = "quit";
       };
     };
   };
